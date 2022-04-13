7291fa77ef86d2905d407f2e31b8e5461b618560
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-04-13 13:03:15 -0500

    suggested search country

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -6,12 +6,20 @@ import { Country } from '../../interfaces/contry.interface';
 @Component({
   selector: 'app-by-country',
   templateUrl: './by-country.component.html',
+  styles: [`
+    li {
+      cursor: pointer;
+    }
+  `
+  ]
 })
 export class ByCountryComponent {
 
-  term: string = '';
-  hasError: boolean = false;
-  countries: Country[] = [];
+  countries         : Country[] = [];
+  hasError          : boolean = false;
+  showSuggested     : boolean = false;
+  suggestedCountries: Country[] = [];
+  term              : string = '';
 
 
   constructor( private countryService: CountryService ) { }
@@ -19,8 +27,9 @@ export class ByCountryComponent {
 
 
   search( term: string ): void {
-    this.hasError = false;
-    this.term     = term;
+    this.hasError       = false;
+    this.showSuggested  = false;
+    this.term           = term;
 
     this.countryService.serachContry( term )
       .subscribe( ( countries ) => {
@@ -35,7 +44,28 @@ export class ByCountryComponent {
 
 
   suggestion( term:string ) {
+
+    this.hasError = false;
+    this.term     = term;
+    this.showSuggested = true;
+    this.suggestedCountries = [];
+    if( term.length < 1 ) return;
+
     this.hasError = false;
 
+    this.countryService.serachContry( term )
+      .subscribe({
+        next: (countries) => {
+          this.suggestedCountries = countries.splice(0,3);
+        },
+        error: (error) => {
+          this.hasError = true;
+        }
+      })
   }
+
+  suggestedSearch( term: string ) {
+    this.search( term );
+  }
+
 }
